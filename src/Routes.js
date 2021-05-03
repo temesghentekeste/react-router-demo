4194347e1c30fa883f78c0dc1c7014f0697caeae
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-05-03 22:41:12 +0300

    Add Users component and route

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 import Profile from './Profile';
+import Users from './Users';
 
 const Routes = () => {
   return (
@@ -9,6 +10,7 @@ const Routes = () => {
       <Switch>
         <Route path="/" component={App} exact />
         <Route path="/profile" component={Profile} />
+        <Route path="/users" component={Users} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/Users.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Users = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello from Users</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Users;
